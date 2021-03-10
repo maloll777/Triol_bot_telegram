daf837275edc5403a9ff4d6b34f361d0ceccfc74
DMITRY LOKHMATOV <maloll777@gmail.com>
2021-03-10 21:00:14 +0300

    Обновлена БД

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ bot = telebot.TeleBot(CONFIG.TOKEN)
 @bot.message_handler(content_types=["text"])
 def send_info_product(message):
     id_item = message.text
+    if len(id_item) > 9:
+        # Ограничение на длину id товара более 9 знаков
+        return 0
+
     bot.send_message(message.chat.id, get_item_info(id_item))
     bot.send_photo(message.chat.id, get_image_product(get_page_product(id_item)))
 
